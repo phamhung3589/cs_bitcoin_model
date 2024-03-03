e56d0e226b0a6ecdb4848fc6cef8d57904bd3584
hungph <hungph@vng.com.vn>
2024-03-03 22:01:20 +0700

    fix(fix bug import):

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='cs_bitcoin_model',
-    version='0.0.3',
+    version='0.0.5',
     keywords=("cs_bitcoin_model", "cs", "bitcoin model"),
     url="https://github.com/phamhung3589/cs_bitcoin_model.git",
     author='hungph',
@@ -10,6 +10,7 @@ setup(
     packages=find_packages(),
     license="MIT Licence",
     platforms="any",
+    package_data={'': ['nginx.cryptosloth.io.crt', 'nginx.cryptosloth.io.key', 'production.config', 'test.config', 'Binance_BTCUSDT_1h.csv']},
     include_package_data=True,
     install_requires=['numpy', 'uvicorn', 'binance.py', 'pandas', 'requests', 'fastapi', 'pydantic'],
     python_requires='>=3.6',
